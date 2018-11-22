4e1124d2fd0c6b22c6aa5bd200ec54dc0319304c
chatpeth <36184704+chatpeth@users.noreply.github.com>
2018-11-22 16:37:53 +0700

    first test

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res){
-  console.log("Got a GET request for the homepage");
-  res.send('Hello GET');
+
+app.use(express.static('public'));
+
+
+app.get('/index', function(req, res){
+  console.log(__dirname);
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
-app.post('/', function(req, res){
-  console.log("Got a POST request for the homepage");
-  res.send('Hello POST');
+app.get('/chart', function(req, res){
+        console.log("got req chart");
+        res.sendFile(__dirname + "/" + "chart.html")
 })
 
-app.delete('/del_user', function(req, res){
-  console.log("Got a GET request for /del_user");
-  res.send('Hello DELETE');
-})
-
-app.get('/list_user', function(req, res){
-  console.log("Got a GET request for /list_user");
-  res.send('Page Listing');
-})
-
-app.get('/ab*cd', function(req, res){
-  console.log("Got a GET request for /ab*cd");
-  res.send('Page Pattern Match');
+app.get('/process_get', function(req, res){
+  response = {
+    first_name:req.query.first_name,
+    last_name:req.query.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 var server = app.listen(8081, function(){
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Server running on port 8081");
 })
