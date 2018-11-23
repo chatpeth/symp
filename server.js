4cdb8ac9eb2c01f71b0b83e088ae7ab2f7c8e5c0
chatpeth <36184704+chatpeth@users.noreply.github.com>
2018-11-23 10:08:40 +0700

    second release

--- server.js
+++ server.js
@@ -17,21 +17,33 @@ server.get('/', function(req, res){
 
 //login
 server.get('/process_get', function(req, res){
+	/*
   response = {
     first_name:req.query.first_name,
     last_name:req.query.last_name
   };
   console.log(response);
   res.end(JSON.stringify(response));
+  */
+  console.log(req.query.uname);
+  console.log(req.query.pwd);
+  if((req.query.uname == "chatpeth") && (req.query.pwd == "pweng_0406"))
+  {
+	  res.sendFile(__dirname + "/" + "chart.html");
+  }
+  else
+  {
+		response = {
+		username: "Incorect username or password of " +req.query.uname,
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+  }
+  
 });
 
-server.get('/lamp', function(req, res){
-	console.log("req lamp received");
-	res.sendFile(__dirname + "/" + "chart.html");
 
-});
-
-server.use((req, res) => res.sendFile(INDEX) );
+//server.use((req, res) => res.sendFile(INDEX) );
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
