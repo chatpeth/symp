d6947a44ddd02f0e2b2470bef5490bdc6baa604e
chatpeth <36184704+chatpeth@users.noreply.github.com>
2018-11-23 09:28:52 +0700

    second release

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
+// Re-write 11232018
+var server = express();
+
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'chart.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+server.get('/index', function(req, res){
+  console.log(__dirname);
+  res.sendFile(__dirname + "/" + "index.html");
+});
+server.get('/process_get', function(req, res){
+  response = {
+    first_name:req.query.first_name,
+    last_name:req.query.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
+server.use((req, res) => res.sendFile(INDEX) );
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+  
 
 const wss = new SocketServer({ server });
 
