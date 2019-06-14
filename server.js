46c20ca3651227280e336d94747f4b05866e1e9c
chatpeth <chatpeth@atop.com.tw>
2019-06-14 22:09:46 +0700

    reupload homepage

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ server.get('/', function(req, res){
   res.sendFile(__dirname + "/" + "homepage.html");
 });
 
+server.get('/home', function(req, res){
+  
+  res.sendFile(__dirname + "/" + "homepage.html");
+});
+
 server.get('/index', function(req, res){
   console.log(__dirname);
   res.sendFile(__dirname + "/" + "index.html");
@@ -81,6 +86,107 @@ server.get('/5610110106', function(req, res){
   console.log("req 5610110106 received");
   res.sendFile(__dirname + "/5610110106.html");
 });
+
+server.get('/rm315', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room315.html");
+});
+
+server.get('/rm316', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room316.html");
+});
+
+server.get('/rm317', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room317.html");
+});
+
+server.get('/rm318', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room318.html");
+});
+
+server.get('/rm321', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room321.html");
+});
+
+server.get('/rm322', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room322.html");
+});
+
+server.get('/rm323', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room323.html");
+});
+
+server.get('/rm324', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room324.html");
+});
+
+server.get('/rm325', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room325.html");
+});
+
+server.get('/rm326', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room326.html");
+});
+
+server.get('/rm327', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room327.html");
+});
+
+server.get('/rm328', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room328.html");
+});
+
+server.get('/rm331', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room331.html");
+});
+
+server.get('/rm332', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room332.html");
+});
+
+server.get('/rm333', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room333.html");
+});
+
+server.get('/rm334', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room334.html");
+});
+
+server.get('/rm335', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room335.html");
+});
+
+server.get('/rm336', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room336.html");
+});
+
+server.get('/rm337', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room337.html");
+});
+
+server.get('/rm338', function(req, res){
+  
+  res.sendFile(__dirname + "/phuket/room338.html");
+});
+
 
 //login
 server.post('/home', function(req, res){
